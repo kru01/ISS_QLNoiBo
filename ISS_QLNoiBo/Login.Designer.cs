08374be5d083fa5a043073dd745b1af4fc2895aa
kru01 <79639749+kru01@users.noreply.github.com>
2024-02-29 23:19:58 +0700

    Initial setup

---
+++ ISS_QLNoiBo/Login.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace ISS_QLNoiBo
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            username = new TextBox();
+            password = new TextBox();
+            loginButton = new Button();
+            SuspendLayout();
+            // 
+            // username
+            // 
+            username.Location = new Point(439, 172);
+            username.Name = "username";
+            username.Size = new Size(125, 27);
+            username.TabIndex = 0;
+            // 
+            // password
+            // 
+            password.Location = new Point(439, 249);
+            password.Name = "password";
+            password.Size = new Size(125, 27);
+            password.TabIndex = 1;
+            // 
+            // loginButton
+            // 
+            loginButton.Location = new Point(448, 316);
+            loginButton.Name = "loginButton";
+            loginButton.Size = new Size(94, 29);
+            loginButton.TabIndex = 2;
+            loginButton.Text = "Login";
+            loginButton.UseVisualStyleBackColor = true;
+            loginButton.Click += LoginButton_Click;
+            // 
+            // Login
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(loginButton);
+            Controls.Add(password);
+            Controls.Add(username);
+            Name = "Login";
+            Text = "Login";
+            Load += Login_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox username;
+        private TextBox password;
+        private Button loginButton;
+    }
+}
---
+++ ISS_QLNoiBo/Login.cs
@@ -0,0 +1,20 @@
+namespace ISS_QLNoiBo
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
