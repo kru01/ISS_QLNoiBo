3fca8f1bdc8358fdbc9988c147ef50f6d520a0b8
kru01 <79639749+kru01@users.noreply.github.com>
2024-03-01 19:20:04 +0700

    SQL setup - added createDB, data, and users scripts - added Admin_main form - modified App.config, and added OracleConfig - modified Login logic

--- ISS_QLNoiBo/Login.cs
+++ ISS_QLNoiBo/Login.cs
@@ -1,3 +1,5 @@
+﻿using Oracle.ManagedDataAccess.Client;
+
 namespace ISS_QLNoiBo
 {
     public partial class Login : Form
@@ -14,7 +16,34 @@ namespace ISS_QLNoiBo
 
         private void LoginButton_Click(object sender, EventArgs e)
         {
+            if (username.Text == "" || password.Text == "")
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!");
+            } else
+            {
+                string connString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)" +
+                    $"(HOST={OracleConfig.host})(PORT={OracleConfig.port})))(CONNECT_DATA=(SERVER=DEDICATED)" +
+                    $"(SERVICE_NAME={OracleConfig.service})));User Id={username.Text};Password={password.Text};";
 
+                OracleConnection conn = new(connString);
+
+                try
+                {
+                    conn.Open();
+                    this.Hide();
+
+                    if (username.Text[..2] == "AD")
+                    {
+                        Admin_main form = new();
+                        form.Show();
+                    }
+
+                    this.Close();
+                } catch (Exception ex)
+                {
+                    MessageBox.Show("ID hoặc mật khẩu sai! " + ex.Message);
+                } finally { conn.Close(); }
+            }
         }
     }
 }
---
+++ ISS_QLNoiBo/OracleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISS_QLNoiBo
+{
+    public static class OracleConfig
+    {
+        public const String host = "localhost";
+        public const String port = "1521";
+        public const String service = "XEPDB1";
+        public const String connString = "localhost:1521/XEPDB1";
+    }
+}
